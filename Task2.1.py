e35be494057c70aa71eacba0f56a98e62ba2bdc2
Kate <katya.prost432@gmail.com>
2021-07-01 12:54:13 +0300

    dop

--- Task2.1.py
+++ Task2.1.py
@@ -1,6 +1,7 @@
 from random import randint
 my_list = [randint(1, 100) for i in range(100)]
 my_list.sort()
-for i in range(0,len(my_list)-1):
-               if my_list[i] == my_list[i+1]:
-                   print(str(my_list[i]) + ' duplicate')
+def lst():
+    for i in range(0,len(my_list)-1):
+         if my_list[i] == my_list[i+1]:
+              print(str(my_list[i]) + ' duplicate')
