a6e06c15382171e78377a284c7a08ceb9634ac28
Kate <katya.prost432@gmail.com>
2021-07-01 14:53:52 +0300

    season

---
+++ Task2.3.py
@@ -0,0 +1,10 @@
+def season(a):
+    if a >0 and a <3 and a == 12:
+        print("Зима")
+    elif a >= 3 and a <=5:
+        print("Весна")
+    elif a >5 and a <=8:
+        print("лето")
+    elif a > 8 and a <=11:
+        print("Осень")
+print(season(3))
