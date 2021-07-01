008e78364d4e43be91dd51631dc3fc5998edfa52
Kate <katya.prost432@gmail.com>
2021-07-01 16:25:12 +0300

    tre

--- Task2.8.py
+++ Task2.8.py
@@ -2,7 +2,7 @@ def sum_range(start, end):
     if start > end:
         end, start = start, end
         return sum(range(start, end + 1))
-    print(sum)
+
 
 start = int(input())
 end = int(input())
