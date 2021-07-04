583c0192c09fb9df4b2fad2fd3be8d34fce15c43
Kate <katya.prost432@gmail.com>
2021-07-05 00:24:30 +0300

    upgrade for Task4.1

--- Task2.10.py
+++ Task2.10.py
@@ -1,14 +1,30 @@
 a = [16, -17, 2, 78.7, False, False, {'True': True}, 555, 12, 23, 42,'DD']
 b = []
+l = []
+a.remove(False)
+a.remove(False)
 
-a.remove(False)
-a.remove(False)
+def time(func):
+    import time
+    def wrap():
+        start = time.time()
+        func ()
+        end = time.time()
+        print(start,end)
+    return wrap
+
+@time
 def lst():
-    for i in a:
-        if isinstance(i, int):
-            b.append(i)
-        elif isinstance(i, float):
-            b.append(i)
-    print(sorted(b))
+        for i in a:
+            if isinstance(i, int):
+                b.append(i)
+            elif isinstance(i, float):
+                b.append(i)
+        print(sorted(b))
 
 lst()
+
+
+
+
+
